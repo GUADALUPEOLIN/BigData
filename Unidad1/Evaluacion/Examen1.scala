3454cf972a6fd594a13b0a9fb6a751d4b8109935
GUADALUPEOLIN <guadalupe.olin18@tectijuana.edu.mx>
2019-09-11 15:04:38 -0700

    agrando la unidad1

---
+++ Unidad1/Evaluacion/Examen1.scala
@@ -0,0 +1,25 @@
+def breakingRecords(games:Array[Int]): List[Int] = { 
+
+var max = games(0) 
+var countMax = 0 
+var min = games(0) 
+var countMin = 0 
+for ( i <- games) { 
+if ( i > max ){ git status
+
+max = i
+countMax += 1 
+}else if( i < min) { 
+min = i 
+countMin += 1
+}
+}
+var MyList = List(countMax,countMin)
+return MyList 
+}
+var totalgames = 9
+var games = Array(10,5,20,20,4,5,2,25,1) 
+var games2 = Array(3,4,21,36,10,28,35,5,24,42)
+
+println(breakingRecords(games)) 
+println(breakingRecords(games2))
---
+++ Unidad1/Practicas/Practica 1.scala
@@ -0,0 +1,31 @@
+//Determinar el radio 
+def radio(Area:Double):Double={
+ (math.sqrt(Area/3.1416))
+}
+radio(28.45)
+
+
+// 
+def isPrime(num:Int):Boolean =
+(num>1)&& !(2 to scala.math.sqrt(num).toInt).exists(x =>num %x ==0)
+
+
+var brird = "tweet"
+println(s"Estoy escribiendo un sbrird")
+
+
+var mensaje= "hola Luke soy tu padre"
+
+mensaje.slice(5,9)
+
+val num=1
+num3=4
+var num2=2
+var num2=3
+
+
+val tupla=(2,4,5),(1,2,3),(3.1416,23))
+tupla
+println(tupla.3_.1)
+println(tupla._3_.1)
+println(tupla._3._1)
